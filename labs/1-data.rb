a696afb857d9d58249543ab39ee0828820b94548
kellyaucoin <kelly.aucoin@kellogg.northwestern.edu>
2023-01-24 00:49:01 +0000

    Completed lab 1-data.rb

--- labs/1-data.rb
+++ labs/1-data.rb
@@ -10,6 +10,15 @@
 # then write them out to the screen. Show the total
 # as well.
 
+die_roll_1 = rand(1..6)       # this gives us a random number between 1 and 6
+puts die_roll_1
+
+die_roll_2 = rand(1..6)       # this gives us a second random number between 1 and 6
+puts die_roll_2
+
+total = die_roll_1 + die_roll_2
+puts "Total: #{total}"
+
 # HINTS
 # Get a random number between 1 and 6 by doing:
 # rand(1..6)
